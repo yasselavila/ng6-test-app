2514a6cb6c73a61e9d121f5abc57c34128a259fb
Yassel Avila <yassel.avila@gmail.com>
2018-04-13 15:20:43 -0400

    Run app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { environment } from '@app/env';
+import { RootModule } from '@app/root';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(RootModule)
   .catch(err => console.log(err));
