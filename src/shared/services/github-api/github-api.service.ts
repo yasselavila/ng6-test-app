4c8de293267a41eb968bb3f51b93f68ec2faa6ff
Yassel Avila <yassel.avila@gmail.com>
2018-04-14 13:37:50 -0400

    Github API service

--- src/shared/services/github-api/github-api.service.ts
+++ src/shared/services/github-api/github-api.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import { Repo } from '../../models';
+
+const githubApiToken: string = 'e1ec192bcec4419511111d75626c7290d9553aba';
+const githubApiUrl: string = 'https://api.github.com';
 
 @Injectable()
 export class GithubApiService {
-  public constructor(private http: HttpClient) {
+  public constructor(private httpClient: HttpClient) {
+  }
+
+  private runQuery(endPoint: string): Observable<Repo[]> {
+    const apiUrl: string = `${githubApiUrl}${endPoint}`;
+
+    const headers: HttpHeaders = new HttpHeaders({
+      'Authorization': `Bearer ${githubApiToken}`,
+      'Accept': 'application/vnd.github.v3+json'
+    });
+
+    return this.httpClient
+      .get<Repo[]>(apiUrl, { headers })
+      .pipe(
+        take(1)
+      );
+  }
+
+  public getUserRepos(username: string): Observable<Object> {
+    return this.runQuery(`/users/${username}/repos`);
   }
 }
