b97f56fe51acb0202358bb91a6645872a7b5331e
Yassel Avila <yassel.avila@gmail.com>
2018-04-14 13:38:43 -0400

    Home Page

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,17 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-import { SharedModule } from '@app/shared/shared.module';
-
 @Component({
   selector: 'app-home-page',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomePageComponent implements OnInit {
-  public constructor() {
-  }
-
-  public ngOnInit(): void {
-    //
-  }
+export class HomePageComponent {
 }
