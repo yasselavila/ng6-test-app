5a318884f11dd67e3871af2054e61f610115f159
Yassel Avila <yassel.avila@gmail.com>
2018-04-14 14:41:09 -0400

    Improve the timeAgo pipe

--- src/shared/pipes/time-ago/time-ago.pipe.ts
+++ src/shared/pipes/time-ago/time-ago.pipe.ts
@@ -2,10 +2,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'timeAgo' })
 export class TimeAgoPipe implements PipeTransform {
-  public transform(value: number, ...args: any[]): string {
-    const date: Date = new Date();
+  public transform(value: Date|number|string, ...args: any[]): string {
+    let date: Date = new Date();
+
+    if ('string' === typeof value) {
+      date.setTime(Date.parse(value));
+    } else if ('number' === typeof value) {
+      date.setTime(value);
+    } else {
+      date = value as Date;
+    }
+
+    value = +date;
+
+    const currDate: Date = new Date();
     const aDay: number = (24 * 60 * 60 * 1000);
-    const seconds: number = Math.floor(((+date) - value) / 1000);
+    const seconds: number = Math.floor(((+currDate) - value) / 1000);
     let interval: number = Math.floor(seconds / 31536000);
 
     if (interval > 1) {
